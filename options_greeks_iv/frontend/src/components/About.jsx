d7fc6350d1fdc9677ff5e30f587a581979547dc1
Akash Mishra <167092640+Akashmishra06@users.noreply.github.com>
2025-06-11 06:36:09 +0000

    changes

--- options_greeks_iv/frontend/src/components/About.jsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>About Page</h2>
-      <p>This is a demo app to view IV charts and data tables based on CSV data by date.</p>
-    </div>
-  );
-}
--- options_greeks_iv/frontend/src/components/Contact.jsx
+++
@@ -1,8 +0,0 @@
-export default function Contact() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>Contact Page</h2>
-      <p>You can contact us at <a href="mailto:support@example.com">support@example.com</a>.</p>
-    </div>
-  );
-}
